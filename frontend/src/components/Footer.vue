d2ecda94989a2c2c6d2c3c935e1c4fe5edc4b697
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-05-30 18:33:08 +0600

    feat: add copy link functionality

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import BaseButton from '@/components/BaseButton.vue';
 import { IconClipboardFilled } from '@tabler/icons-vue';
+import { useShareLink } from '@/composables/useShareLink';
+
+const { copyLink } = useShareLink();
 </script>
 
 <template>
   <footer class="footer">
-    <BaseButton variant="secondary" class="footer__button footer__button--copy">
+    <BaseButton
+      variant="secondary"
+      @click="copyLink"
+      class="footer__button footer__button--copy"
+    >
       <IconClipboardFilled />
       Скопировать ссылку
     </BaseButton>
