3f5147dac452c7ca83dae9234e9bf03c84e9e2fd
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-05-30 17:45:07 +0600

    feat: add Footer component, remove Navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@ import Header from '@/components/Header.vue';
 import Navigation from '@/components/Navigation.vue';
 import { useDishes } from '@/composables/useDishes';
 import { onMounted, provide } from 'vue';
+import Footer from '@/components/Footer.vue';
 
 const useDishesData = useDishes();
 provide('dishes', useDishesData);
@@ -15,22 +16,23 @@ onMounted(async () => {
 <template>
   <div class="page">
     <Header />
-    <Navigation />
     <main class="page__main">
       <RouterView />
     </main>
+    <Footer />
   </div>
 </template>
 
 <style lang="scss">
 .page {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
   &__main {
-    flex: 1;
     width: 100%;
-    background: var(--color-white);
-    border-radius: 1rem;
-    border: 1px solid var(--color-border);
-    padding: 1.5rem 1.5rem;
+    flex: 1;
+    background: var(--color-light-lavender);
   }
 }
 </style>
