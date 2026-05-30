8b22acd311c7e9bd314a848135a4c8d45230ea42
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-05-30 17:17:05 +0600

    style: update global styles and header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,22 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IconMoonFilled, IconSparkle } from '@tabler/icons-vue';
+import BaseButton from '@/components/BaseButton.vue';
+</script>
+
 <template>
   <header class="header">
     <div class="header__logo">
-      <span class="header__icon">✦</span>
+      <IconSparkle class="header__icon" stroke="1.8" />
       <h1 class="header__title">Bill Splitter</h1>
     </div>
+
     <p class="header__tagline">Разделите счет честно и без споров</p>
+
+    <BaseButton variant="icon" class="header__theme-toggle">
+      <!--onclick-->
+      <IconMoonFilled />
+    </BaseButton>
   </header>
 </template>
 
 <style lang="scss">
 .header {
-  padding: 1.5rem 0;
-  position: sticky;
+  position: relative;
   display: flex;
+  width: 100%;
+  padding: 1rem 1rem 2rem;
+  justify-content: space-between;
   flex-direction: column;
-  align-items: flex-start;
-  gap: 0.6rem;
+  align-items: center;
+  gap: 0.2rem;
+  background-color: var(--color-white);
+  border-bottom: 0.1rem solid var(--color-light-purple-gray);
 
   &__logo {
     display: flex;
@@ -25,22 +39,35 @@
   }
 
   &__icon {
-    font-size: 1.5rem;
     color: var(--color-primary);
   }
 
   &__title {
     font-family: var(--font-heading), serif;
+    color: var(--color-primary);
     font-size: 2rem;
-    font-weight: 400;
+    font-weight: var(--font-weight-regular);
     letter-spacing: -0.03rem;
   }
 
   &__tagline {
-    font-size: 0.9rem;
-    color: var(--color-gray);
-    font-weight: 300;
-    padding-left: 1.5rem;
+    font-size: var(--font-size);
+    font-weight: var(--font-weight-regular);
+    text-align: center;
+    color: var(--color-muted-purple);
+  }
+
+  &__theme-toggle {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0.6rem;
+    gap: 0.5rem;
+    color: var(--color-muted-purple);
+    border: 0.1rem solid var(--color-light-purple-gray);
+    border-radius: var(--border-radius-lg);
   }
 }
 </style>
