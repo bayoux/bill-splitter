798a6dcc602876f04fed4c5d7fa66e75d8e27bbf
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-05-30 17:11:40 +0600

    feat: add Footer and QrUpload components

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue';
+import { IconClipboardFilled } from '@tabler/icons-vue';
+</script>
+
+<template>
+  <footer class="footer">
+    <BaseButton variant="secondary" class="footer__button footer__button--copy">
+      <IconClipboardFilled />
+      Скопировать ссылку
+    </BaseButton>
+
+    <BaseButton variant="primary" class="footer__button footer__button--save">
+      Сохранить
+    </BaseButton>
+  </footer>
+</template>
+
+<style lang="scss">
+.footer {
+  display: flex;
+  width: 100%;
+  padding: 1rem 1rem 2rem;
+  align-items: center;
+  justify-content: center;
+  margin-top: auto;
+  gap: 1rem;
+  background-color: var(--color-white);
+
+  &__button {
+    flex: 1;
+    padding: 1rem;
+    border-radius: var(--border-radius-lg);
+    height: 3.5rem;
+  }
+}
+</style>
---
+++ frontend/src/components/QrUpload.vue
@@ -0,0 +1,208 @@
+<script setup lang="ts">
+import { useQrCode } from '@/composables/useQrCode';
+import { onMounted } from 'vue';
+import BaseButton from '@/components/BaseButton.vue';
+import { IconFileUploadFilled } from '@tabler/icons-vue';
+const {
+  qrSrc,
+  fileName,
+  fileSize,
+  onQrUpload,
+  deleteQrCode,
+  getQrCode,
+  showQrCode,
+} = useQrCode();
+
+onMounted(async () => {
+  await getQrCode();
+});
+</script>
+
+<template>
+  <div class="qr-upload">
+    <template v-if="!qrSrc">
+      <div class="qr-upload__qr">
+        <IconFileUploadFilled class="qr-upload__icon qr-upload__icon--upload" />
+        <div>
+          <p class="qr-upload__qr-title">Загрузить QR</p>
+          <p class="qr-upload__qr-subtitle">PNG, JPG, JPEG</p>
+        </div>
+      </div>
+
+      <label class="qr-upload__upload-label" for="qr-upload">Загрузить </label>
+
+      <input
+        class="qr-upload__file-input"
+        id="qr-upload"
+        type="file"
+        accept="image/*"
+        @change="onQrUpload"
+        style="display: none"
+      />
+    </template>
+
+    <template v-else>
+      <div class="qr-upload__qr-file">
+        <img
+          class="qr-upload__qr-image"
+          :src="qrSrc"
+          alt="QR"
+          @click="showQrCode = true"
+        />
+
+        <div
+          v-if="showQrCode"
+          class="qr-upload__qr-overlay"
+          @click="showQrCode = false"
+        >
+          <div class="qr-upload__qr-popup" @click.stop>
+            <img :src="qrSrc" alt="QR" />
+          </div>
+        </div>
+
+        <div class="qr-upload__qr-info">
+          <p class="qr-upload__qr-name">{{ fileName }}</p>
+          <p class="qr-upload__qr-size">{{ fileSize }}</p>
+        </div>
+      </div>
+
+      <BaseButton
+        variant="secondary"
+        @click="deleteQrCode"
+        class="qr-upload__button qr-upload__button--delete"
+      >
+        Удалить
+      </BaseButton>
+    </template>
+  </div>
+</template>
+
+<style lang="scss">
+.qr-upload {
+  width: 100%;
+  max-width: 34rem;
+  min-height: 5rem;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: var(--border-radius-md);
+  background-color: var(--color-white);
+
+  &__qr {
+    display: flex;
+    align-items: center;
+  }
+
+  &__icon {
+    color: var(--color-primary);
+    &--upload {
+      width: var(--icon-lg);
+      height: var(--icon-lg);
+    }
+  }
+
+  &__qr-title {
+    font-size: 1rem;
+    font-weight: var(--font-weight-semibold);
+    margin-bottom: 0.3rem;
+    color: var(--color-muted-purple);
+  }
+
+  &__qr-subtitle {
+    font-size: 0.8rem;
+    font-weight: var(--font-weight-regular);
+    color: var(--color-muted-purple);
+  }
+
+  &__upload-label {
+    display: flex;
+    justify-content: center;
+    padding: 0.7rem 1.5rem;
+    max-width: 7rem;
+    min-height: 2.5rem;
+    margin-left: auto;
+    font-weight: var(--font-weight-medium);
+    color: var(--color-muted-purple);
+    background-color: var(--color-secondary);
+    border-radius: var(--border-radius-lg);
+    border: 0.1rem solid transparent;
+
+    &:hover {
+      border-color: var(--color-primary);
+    }
+  }
+
+  &__file-input {
+    display: none;
+  }
+
+  &__qr-file {
+    max-width: 34rem;
+    min-height: 3.4rem;
+    display: flex;
+    letter-spacing: 0.01em;
+    line-height: 1.7;
+    gap: 1rem;
+    flex: 1;
+  }
+
+  &__qr-image {
+    width: 3.5rem;
+    height: 3.5rem;
+    object-fit: cover;
+    border-radius: 0.1rem;
+    cursor: pointer;
+  }
+
+  &__qr-overlay {
+    position: fixed;
+    inset: 0;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+  }
+
+  &__qr-popup {
+    border-radius: var(--border-radius-lg);
+    padding: 1.5rem;
+
+    img {
+      max-width: 18rem;
+      height: auto;
+      object-fit: contain;
+      border-radius: var(--border-radius-md);
+    }
+  }
+
+  &__qr-info {
+    color: var(--color-black);
+  }
+
+  &__qr-name {
+    font-size: var(--font-size);
+    font-weight: var(--font-weight-semibold);
+    color: var(--color-muted-purple);
+  }
+
+  &__qr-size {
+    font-size: 0.8rem;
+    font-weight: var(--font-weight-semibold);
+    letter-spacing: 0.1rem;
+    color: var(--color-muted-purple);
+  }
+
+  &__button {
+    border-radius: var(--border-radius-lg);
+    &--delete {
+      width: 100%;
+      max-width: 7rem;
+      min-height: 2.7rem;
+      padding: 0.7rem 1.5rem;
+      font-weight: var(--font-weight-medium);
+    }
+  }
+}
+</style>
