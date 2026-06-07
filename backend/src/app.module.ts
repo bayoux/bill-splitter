6ac030a9a964ae2baa32cc459c4d4af20f198918
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2026-06-08 01:25:44 +0600

    feat: add sessions entities to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,11 @@ import { Dish } from './dishes/dish.entity';
 import { ConfigModule } from '@nestjs/config';
 import { QrCodeModule } from './qr-code/qr-code.module';
 import { QrCode } from './qr-code/qr-code.entity';
+import { Session } from './sessions/session.entity';
+import { SessionDish } from './sessions/session-dish.entity';
+import { Selection } from './sessions/selection.entity';
+import { Participant } from './sessions/participant.entity';
+import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
   imports: [
@@ -16,11 +21,12 @@ import { QrCode } from './qr-code/qr-code.entity';
       username: process.env.DB_USERNAME!,
       password: process.env.DB_PASSWORD!,
       database: process.env.DB_NAME!,
-      entities: [Dish, QrCode],
+      entities: [Dish, QrCode, Session, SessionDish, Selection, Participant],
       synchronize: process.env.NODE_ENV !== 'production',
     }),
     DishesModule,
     QrCodeModule,
+    SessionsModule,
   ],
 })
 export class AppModule {}
